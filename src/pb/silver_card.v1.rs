c0797bdf07f7e9a3f54a4c131df7f2bb1985b565
Ashish Sapkota <aacspkt3@gmail.com>
2025-02-07 04:17:10 +0000

    fix: update proto messages according to changes in solana program

--- src/pb/silver_card.v1.rs
+++ src/pb/silver_card.v1.rs
@@ -16,118 +16,205 @@ pub struct Output {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Deposit {
-    #[prost(string, tag="1")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub depositor: ::prost::alloc::string::String,
+    #[prost(uint64, tag="1")]
+    pub slot: u64,
+    #[prost(uint64, tag="2")]
+    pub block_height: u64,
     #[prost(string, tag="3")]
-    pub user_vault: ::prost::alloc::string::String,
-    #[prost(string, tag="4")]
-    pub input_token: ::prost::alloc::string::String,
+    pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
     #[prost(string, tag="5")]
-    pub output_token: ::prost::alloc::string::String,
+    pub tx_hash: ::prost::alloc::string::String,
     #[prost(uint64, tag="6")]
     pub input_amount: u64,
     #[prost(uint64, tag="7")]
     pub output_amount: u64,
-    #[prost(enumeration="DepositType", tag="8")]
+    #[prost(string, tag="8")]
+    pub input_token: ::prost::alloc::string::String,
+    #[prost(string, tag="9")]
+    pub output_token: ::prost::alloc::string::String,
+    #[prost(enumeration="DepositType", tag="10")]
     pub deposit_type: i32,
-    #[prost(int64, tag="9")]
-    pub timestamp: i64,
-    #[prost(uint64, tag="10")]
-    pub slot: u64,
-    #[prost(uint64, tag="11")]
-    pub block_height: u64,
+    #[prost(string, tag="11")]
+    pub depositor: ::prost::alloc::string::String,
     #[prost(string, tag="12")]
-    pub blockhash: ::prost::alloc::string::String,
+    pub user_vault: ::prost::alloc::string::String,
+    #[prost(string, tag="13")]
+    pub purchase_record: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Withdraw {
-    #[prost(string, tag="1")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub user_vault: ::prost::alloc::string::String,
-    #[prost(string, tag="3")]
-    pub withdrawer: ::prost::alloc::string::String,
-    #[prost(string, tag="4")]
-    pub token: ::prost::alloc::string::String,
-    #[prost(uint64, tag="5")]
-    pub amount: u64,
-    #[prost(int64, tag="6")]
-    pub timestamp: i64,
-    #[prost(uint64, tag="7")]
+    #[prost(uint64, tag="1")]
     pub slot: u64,
-    #[prost(uint64, tag="8")]
+    #[prost(uint64, tag="2")]
     pub block_height: u64,
-    #[prost(string, tag="9")]
+    #[prost(string, tag="3")]
     pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(uint64, tag="6")]
+    pub amount: u64,
+    #[prost(string, tag="7")]
+    pub token: ::prost::alloc::string::String,
+    #[prost(string, tag="8")]
+    pub user_vault: ::prost::alloc::string::String,
+    #[prost(string, tag="9")]
+    pub withdrawer: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CardPurchase {
-    #[prost(string, tag="1")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(uint64, tag="2")]
-    pub card_id: u64,
-    #[prost(string, tag="3")]
-    pub buyer: ::prost::alloc::string::String,
-    #[prost(string, tag="4")]
-    pub buyer_vault: ::prost::alloc::string::String,
-    #[prost(uint64, tag="5")]
-    pub amount: u64,
-    #[prost(string, tag="6")]
-    pub card_type: ::prost::alloc::string::String,
-    #[prost(int64, tag="7")]
-    pub timestamp: i64,
-    #[prost(uint64, tag="8")]
+    #[prost(uint64, tag="1")]
     pub slot: u64,
-    #[prost(uint64, tag="9")]
+    #[prost(uint64, tag="2")]
     pub block_height: u64,
-    #[prost(string, tag="10")]
+    #[prost(string, tag="3")]
     pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(uint64, tag="6")]
+    pub card_id: u64,
+    #[prost(string, tag="7")]
+    pub card_type: ::prost::alloc::string::String,
+    #[prost(uint64, tag="8")]
+    pub amount: u64,
+    #[prost(string, tag="9")]
+    pub buyer: ::prost::alloc::string::String,
+    #[prost(string, tag="10")]
+    pub buyer_vault: ::prost::alloc::string::String,
+    #[prost(string, tag="11")]
+    pub purchase_record: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct GenerateYield {
-    #[prost(string, tag="1")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub user: ::prost::alloc::string::String,
-    #[prost(string, tag="3")]
-    pub user_vault: ::prost::alloc::string::String,
-    #[prost(uint64, tag="4")]
-    pub amount: u64,
-    #[prost(int64, tag="5")]
-    pub timestamp: i64,
-    #[prost(uint64, tag="6")]
+    #[prost(uint64, tag="1")]
     pub slot: u64,
-    #[prost(uint64, tag="7")]
+    #[prost(uint64, tag="2")]
     pub block_height: u64,
-    #[prost(string, tag="8")]
+    #[prost(string, tag="3")]
     pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(uint64, tag="6")]
+    pub amount: u64,
+    #[prost(string, tag="7")]
+    pub user: ::prost::alloc::string::String,
+    #[prost(string, tag="8")]
+    pub user_vault: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct WithdrawYield {
-    #[prost(string, tag="1")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub user: ::prost::alloc::string::String,
-    #[prost(string, tag="3")]
-    pub user_vault: ::prost::alloc::string::String,
-    #[prost(uint64, tag="4")]
-    pub amount: u64,
-    #[prost(bool, tag="5")]
-    pub withdraw_all: bool,
-    #[prost(int64, tag="6")]
-    pub timestamp: i64,
-    #[prost(uint64, tag="7")]
+    #[prost(uint64, tag="1")]
     pub slot: u64,
-    #[prost(uint64, tag="8")]
+    #[prost(uint64, tag="2")]
     pub block_height: u64,
-    #[prost(string, tag="9")]
+    #[prost(string, tag="3")]
     pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(uint64, tag="6")]
+    pub amount: u64,
+    #[prost(bool, tag="7")]
+    pub withdraw_all: bool,
+    #[prost(string, tag="8")]
+    pub user: ::prost::alloc::string::String,
+    #[prost(string, tag="9")]
+    pub user_vault: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DirectCardPurchase {
+    #[prost(uint64, tag="1")]
+    pub slot: u64,
+    #[prost(uint64, tag="2")]
+    pub block_height: u64,
+    #[prost(string, tag="3")]
+    pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(string, tag="6")]
+    pub input_token: ::prost::alloc::string::String,
+    #[prost(string, tag="7")]
+    pub output_token: ::prost::alloc::string::String,
+    #[prost(uint64, tag="8")]
+    pub input_amount: u64,
+    #[prost(uint64, tag="9")]
+    pub output_amount: u64,
+    #[prost(uint64, tag="10")]
+    pub card_id: u64,
+    #[prost(string, tag="11")]
+    pub card_type: ::prost::alloc::string::String,
+    #[prost(string, tag="12")]
+    pub buyer: ::prost::alloc::string::String,
+    #[prost(string, tag="13")]
+    pub buyer_purchase: ::prost::alloc::string::String,
+    #[prost(string, tag="14")]
+    pub purchase_record: ::prost::alloc::string::String,
+}
+/// Card Bot Messages
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct InitCardBotUserAccount {
+    #[prost(uint64, tag="1")]
+    pub slot: u64,
+    #[prost(uint64, tag="2")]
+    pub block_height: u64,
+    #[prost(string, tag="3")]
+    pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(string, tag="6")]
+    pub user_id: ::prost::alloc::string::String,
+    #[prost(string, tag="7")]
+    pub user_custody: ::prost::alloc::string::String,
+    #[prost(string, tag="8")]
+    pub usdc_token: ::prost::alloc::string::String,
+    #[prost(string, tag="9")]
+    pub admin: ::prost::alloc::string::String,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CardBotDirectCardPurchase {
+    #[prost(uint64, tag="1")]
+    pub slot: u64,
+    #[prost(uint64, tag="2")]
+    pub block_height: u64,
+    #[prost(string, tag="3")]
+    pub blockhash: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub timestamp: i64,
+    #[prost(string, tag="5")]
+    pub tx_hash: ::prost::alloc::string::String,
+    #[prost(string, tag="6")]
+    pub user_id: ::prost::alloc::string::String,
+    #[prost(string, tag="7")]
+    pub input_token: ::prost::alloc::string::String,
+    #[prost(string, tag="8")]
+    pub output_token: ::prost::alloc::string::String,
+    #[prost(uint64, tag="9")]
+    pub input_amount: u64,
+    #[prost(uint64, tag="10")]
+    pub output_amount: u64,
+    #[prost(string, tag="11")]
+    pub user_custody: ::prost::alloc::string::String,
+    #[prost(string, tag="12")]
+    pub admin: ::prost::alloc::string::String,
 }
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
